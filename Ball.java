2f7431500477cedb070c28523cea87742f015e71
sowmyadr673 <89777877+sowmyadr673@users.noreply.github.com>
2021-09-23 23:40:47 +0530

    Add files via upload

---
+++ Ball.java
@@ -0,0 +1,24 @@
+package com.xworkz.oopsconcepts;
+
+public class Ball {
+
+	String lether;
+	String color;
+	int weight;
+	String usage;
+	String pattern;
+	String round;
+	int price;
+
+	public void kick() {
+		System.out.println("kick the ball");
+	}
+
+	public void throwing() {
+		System.out.println("throw the ball");
+	}
+
+	public void roll() {
+		System.out.println("roll the ball");
+	}
+}
